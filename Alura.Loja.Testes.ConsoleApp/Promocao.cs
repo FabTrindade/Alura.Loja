8bfa855b4343c99132640c9987b2fe22ed0acae3
FabTrindade <fabrtrindade@gmail.com>
2022-04-30 21:43:03 -0300

    Creating a Join table PromocaoProduto to implement many to many ralationship.

---
+++ Alura.Loja.Testes.ConsoleApp/Promocao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    public class Promocao
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFim { get; set; }
+        public IList<PromocaoProduto> Produtos { get; set; }
+    }
+}
--- Readme.md
+++ Readme.md
@@ -1,6 +1,6 @@
 # Alura.Loja. 
 
-Here the idea is:
-_update an app with an old database access system to use the Entity Framework.
-_test Entity Framework features. 
+#### Here the idea is:
+<p>_update an app with an old database access system to use the Entity Framework.</p>
+<p>_test Entity Framework features.</P> 
 
